ba059f2c20b69de22b14f6daae5ca4f5f4552a96
Joosep Ress <joosepress@Jooseps-Air.lan>
2018-08-07 22:42:27 +0300

    bootstrapi algus

--- mainpage.php
+++ mainpage.php
@@ -1,75 +1,26 @@
-<html !DOCTYPE>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="mainpage.css"/>
-    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
-    <script
-      src="https://code.jquery.com/jquery-3.1.1.min.js"
-      crossorigin="anonymous"></script>
-    <script src="semantic/dist/semantic.min.js"></script>
-</head>
-<body>
- <div class="container">
-	<div class="header">
-		<div class="headcontainer">
-			<p class="greeting">Tere tulemast, Jon</p>
-			<p class="co-lunch">CO-LUNCH</p>
-		</div>
-	</div>       
-        <div class="sisucontainer" style"height=800px;">
-            <div class="parempool">
-					<div class="ui selection dropdown">		
-						<div class="default text">Restoran</div>
-						<div class="menu">
-							<div class="item" data-value="1">Restoran 1</div>
-							<div class="item" data-value="0">Restoran 2</div>
-						</div>
-					</div></br>
-					<script>$('.ui.dropdown')
-						.dropdown();</script>			
-					<div class="ui right labeled input">
-						<input type="text">
-						<div class="ui basic label">
-							Tund
-						</div>
-					</div>
-					<div class="ui right labeled input">
-						<input type="text">
-						<div class="ui basic label">
-							Minut
-						</div>
-					</div>
-			</div>
-            <div class="vasakpool">
-				<h3>Lounasoogi pakkumised</h3>
-				<div class="ui raised segment">	
-					<p>Rida 1</p>
-					<p>Rida 2</p>					
-					<p>Rida 3</p>
-					<p>Rida 4</p>
-					<button class="positive ui button">NÕUSTU</button>	
-				</div>
-				
-				<div class="ui raised segment">
-					<p>Rida 1</p>
-					<p>Rida 2</p>
-					<p>Rida 3</p>
-					<p>Rida 4</p>
-					<button class="positive ui button">NÕUSTU</button>			
-				</div>
-				<div class="ui raised segment">
-					<p>Rida 1</p>
-					<p>Rida 2</p>
-					<p>Rida 3</p>
-					<p>Rida 4</p>
-					<button class="positive ui button">NÕUSTU</button>						
-				</div>
-            </div>   
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+    <body>
+        <div class="row">
+            <div class="col-sm-4">.col-sm-4</div>
+            <div class="col-sm-4">.col-sm-4</div>
+            <div class="col-sm-4">.col-sm-4</div>
         </div>
-
-</div>
-    
-</body>
-
-</html> 
-
+        <div class="container-fluid"> 
+            <h1>My First Bootstrap Page</h1>
+            <p>This is some text.</p> 
+        </div>
+    </body>
+</html>
---
+++ vana_mainpage.php
@@ -0,0 +1,76 @@
+<html !DOCTYPE>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mainpage.css"/>
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <script
+      src="https://code.jquery.com/jquery-3.1.1.min.js"
+      crossorigin="anonymous"></script>
+    <script src="semantic/dist/semantic.min.js"></script>
+</head>
+<body>
+ <div class="container">
+	<div class="header">
+		<div class="headcontainer">
+			<p class="greeting">Tere tulemast, Jon</p>
+			<p class="co-lunch">CO-LUNCH</p>
+		</div>
+	</div>       
+        <div class="sisucontainer" style"height=800px;">
+            <div class="parempool">
+				<h2> Postita ise</h2>
+					<div class="ui selection dropdown">			
+						<div class="default text">Restoran</div>
+						<div class="menu">
+							<div class="item" data-value="1">Restoran 1</div>
+							<div class="item" data-value="0">Restoran 2</div>
+						</div>
+					</div></br>
+					<script>$('.ui.dropdown')
+						.dropdown();</script>			
+					<div class="ui right labeled input">
+						<input type="text">
+						<div class="ui basic label">
+							Tund
+						</div>
+					</div>
+					<div class="ui right labeled input">
+						<input type="text">
+						<div class="ui basic label">
+							Minut
+						</div>
+					</div>
+			</div>
+            <div class="vasakpool">
+				<h3>Lounasoogi pakkumised</h3>
+				<div class="ui raised segment">	
+					<p>Rida 1</p>
+					<p>Rida 2</p>					
+					<p>Rida 3</p>
+					<p>Rida 4</p>
+					<button class="positive ui button">NÕUSTU</button>	
+				</div>
+				
+				<div class="ui raised segment">
+					<p>Rida 1</p>
+					<p>Rida 2</p>
+					<p>Rida 3</p>
+					<p>Rida 4</p>
+					<button class="positive ui button">NÕUSTU</button>			
+				</div>
+				<div class="ui raised segment">
+					<p>Rida 1</p>
+					<p>Rida 2</p>
+					<p>Rida 3</p>
+					<p>Rida 4</p>
+					<button class="positive ui button">NÕUSTU</button>						
+				</div>
+            </div>   
+        </div>
+
+</div>
+    
+</body>
+
+</html> 
+
