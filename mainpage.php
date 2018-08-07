2b1e7734dc8662311901bc79126719f2bbb57f35
Joosep Ress <joosepress@Jooseps-Air.lan>
2018-08-07 10:46:42 +0300

    vasak ja parem pool basic

--- mainpage.php
+++ mainpage.php
@@ -18,21 +18,51 @@
 	</div>       
         <div class="sisucontainer" style"height=800px;">
             <div class="parempool">
-				
-            </div>
+					<div class="ui selection dropdown">		
+						<div class="default text">Restoran</div>
+						<div class="menu">
+							<div class="item" data-value="1">Restoran 1</div>
+							<div class="item" data-value="0">Restoran 2</div>
+						</div>
+					</div></br>
+					<script>$('.ui.dropdown')
+						.dropdown();</script>			
+					<div class="ui right labeled input">
+						<input type="number">
+						<div class="ui basic label">
+							Tund
+						</div>
+					</div>
+					<div class="ui right labeled input">
+						<input type="number">
+						<div class="ui basic label">
+							Minut
+						</div>
+					</div>
+			</div>
             <div class="vasakpool">
+				<h3>Lounasoogi pakkumised</h3>
+				<div class="ui raised segment">	
+					<p>Rida 1</p>
+					<p>Rida 2</p>					
+					<p>Rida 3</p>
+					<p>Rida 4</p>
+					<button class="positive ui button">NÕUSTU</button>	
+				</div>
+				
 				<div class="ui raised segment">
 					<p>Rida 1</p>
 					<p>Rida 2</p>
 					<p>Rida 3</p>
 					<p>Rida 4</p>
-					
+					<button class="positive ui button">NÕUSTU</button>			
 				</div>
 				<div class="ui raised segment">
 					<p>Rida 1</p>
 					<p>Rida 2</p>
 					<p>Rida 3</p>
-					<p>Rida 4</p>					
+					<p>Rida 4</p>
+					<button class="positive ui button">NÕUSTU</button>						
 				</div>
             </div>   
         </div>
