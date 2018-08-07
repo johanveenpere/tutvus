6af5a033f5a72d5d0f07f5ee2a5ab703f992262f
Joosep Ress <joosepress@Jooseps-Air.lan>
2018-08-07 09:16:11 +0300

    joosepi veits katkine mainpage

--- mainpage.php
+++ mainpage.php
@@ -1,14 +1,33 @@
-<html>
-	<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"/>
+<html !DOCTYPE>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mainpage.css"/>
     <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
     <script
       src="https://code.jquery.com/jquery-3.1.1.min.js"
       crossorigin="anonymous"></script>
     <script src="semantic/dist/semantic.min.js"></script>
-	</head>
-	<body>
-		<p>logged in</p>
-	</body>
-</html>
+</head>
+<body>
+ <div class="container">
+	<div class="header">
+		<div class="headcontainer">
+			<p class="greeting">Tere tulemast, Jon</p>
+			<p class="co-lunch">CO-LUNCH</p>
+		</div>
+	</div>       
+        <div class="sisucontainer" style"height=800px;">
+            <div class="parempool">
+                        
+            </div>
+            <div class="vasakpool">
+                <p>nwne</p>
+            </div>   
+        </div>
+
+</div>
+    
+</body>
+
+</html> 
+
