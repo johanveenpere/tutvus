2ccbbe3f3e75e1fb3b07eb50f3e162b00425811f
Joosep Ress <joosepress@Jooseps-MacBook-Air.local>
2018-08-10 02:18:44 +0300

    login

--- login.php
+++ login.php
@@ -34,16 +34,23 @@
 	</head>
 
 	<body>
-		<div class="ui container">
-			<p>LOGIN</p>
-			<form action="" class="ui form" method="post">
-						<div class='field'><input type='text' placeholder='username' name='username'></div>
-						<div class='field'><input type='password' placeholder='password' name='password'></div>
-						<button type='submit' class='ui button'>log in</button>
-			</form>
-			<div class="register">
-				<p>don't have an account?</p><a href='register.php?register'>register</a>
+	
+		<div class="ui grid">
+			<div class="fifteen wide centered column">
+				<h4 class="co-lunch">CO-LUNCH</h4>
 			</div>
+			<div class="four wide centered column">
+				<h2>Logi sisse</h2>
+				<div class="ui input">
+  					<input type="text" placeholder="Kasutajanimi">
+				</div>
+				<div class="ui input">
+  					<input type="password" placeholder="Parool">
+				</div>
+				<button class="ui button">
+					Logi sisse
+				</button>
+			</div>	
 		</div>
 	</body>
 </html>
