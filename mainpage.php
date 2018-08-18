2a3b02d2e797aa9ec49d63c62488e246a40a7edd
Joosep Ress <joosepress@Jooseps-Air.lan>
2018-08-18 18:27:32 +0300

    remove "s"

--- mainpage.php
+++ mainpage.php
@@ -29,11 +29,12 @@
 <html>
 	<head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    s	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
 		<script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
 		<script src="semantic/dist/semantic.min.js"></script>
 		<link rel="stylesheet" href="mainpagestyle.css">
         <script src="addLunch.js" type="text/javascript"></script>
+        
         <!--script>
             function showAddLunchPopup(){
                 console.log("show popup");
